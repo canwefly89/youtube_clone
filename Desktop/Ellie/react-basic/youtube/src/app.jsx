940db937443fc1b15c5b430e9873141c6b74ef79
seongju <canwefly89@gmail.com>
2021-02-12 02:45:29 +0900

    search done

--- Desktop/Ellie/react-basic/youtube/src/app.jsx
+++ Desktop/Ellie/react-basic/youtube/src/app.jsx
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB1I58ny5TOq3B2qMwGT1kzamzi6IHog2E",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos))
       .catch((error) => console.log("error", error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
