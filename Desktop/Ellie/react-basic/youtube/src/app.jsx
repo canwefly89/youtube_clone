19d1cff16ac6e632dc621b10e09fe91d88458e2a
seongju <canwefly89@gmail.com>
2021-02-11 23:04:03 +0900

    VideoList, VideoItem, Popular API

---
+++ Desktop/Ellie/react-basic/youtube/src/app.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB1I58ny5TOq3B2qMwGT1kzamzi6IHog2E",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
+}
+
+export default App;
